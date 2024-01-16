e0af1513a77da63ff65f58264269fe4ab6fd0b4f
etahae <tahanamir@hotmail.com>
2024-01-16 22:05:50 +0100

    fixing login layout for centering

--- frontend/src/app/login/layout.tsx
+++ frontend/src/app/login/layout.tsx
@@ -9,8 +9,8 @@ export default function LoginLayout({
     children: React.ReactNode
   }) {
     return (
-    <main className="flex w-full justify-center select-none transition-all min-h-400px sticky overflow-y-clip">
-        <motion.div className='m-16 overflow-hidden w-[600px] min-w-[350px] max-w-[900px] lg:w-[80%] min-h-[34rem] rounded-3xl flex justify-center items-center lg:justify-normal z-0 bg-black border-2 lg:border-black md:min-h-[34rem]' initial={{opacity: 0}} animate={{opacity:1}} exit={{opacity:0}}>
+    <main className="flex w-full items-center justify-center select-none transition-all fixed h-full bg-transparent overflow-y-clip">
+        <motion.div className='mx-16 overflow-hidden w-[600px] min-w-[350px] max-w-[900px] lg:w-[80%] min-h-[37rem] rounded-3xl flex justify-center items-center lg:justify-normal z-0 bg-black border-2 lg:border-black md:min-h-[34rem]' initial={{opacity: 0}} animate={{opacity:1}} exit={{opacity:0}}>
             <div className='rounded-3xl overflow-hidden -z-10 transition-all absolute lg:static opacity-30 lg:opacity-100 lg:block w-[60%] bg-black flex-col justify-center items-start'>
                 <img draggable={false} className='transition-all bg-contain select-none' src='images/loginBG.jpg'></img>
             </div>
